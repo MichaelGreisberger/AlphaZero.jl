1d1915248157a5f1ec139f4b0badd7dbfa9a1d4d
MichaelGreisberger <mike.greisberger@gmail.com>
2021-04-14 13:49:09 +0200

    tests changed

--- games/fanorona/test.jl
+++ games/fanorona/test.jl
@@ -3,7 +3,9 @@ import AlphaZero.Scripts.test_game
 include("game.jl")
 # import AlphaZero
 # include("../../src/scripts/dummy_run.jl")
+# spec, env = run_test()
 # println("TEST GAME")
 # test_game(GameSpec())
 println("DUMMY RUN")
+# dummy_run("connect-four")
 dummy_run("fanorona")
